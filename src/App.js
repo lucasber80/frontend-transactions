12693e1f81aa237b76b20a361bb92e660633f294
lucas bernardo <lucasber80@hotmail.com>
2022-11-17 17:16:45 -0300

    .

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div className="d-flex align-items-center justify-content-center content" style={{ height: "100vh" }}>
-      <SignUp />
+    <div
+      className="d-flex align-items-start justify-content-center content"
+      style={{ height: "100vh" }}
+    >
+      <div className="d-flex flex-column align-items-center justify-content-center">
+        <img src={require("./assets/logo.png")} className="logo" />
+
+        <SignUp />
+      </div>
     </div>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,42 +1,67 @@
 import "./SignUp.css";
 import { useState } from "react";
+import axios from "axios";
 function SignUp() {
-  const [name, SetName] = useState();
-  const [password, Setpassword] = useState();
-  const [confirmPassword, SetconfirmPassword] = useState();
+  const [email, setEmail] = useState("");
+  const [passwordMin, SetPasswordMin] = useState("");
+  const [password, Setpassword] = useState("");
+  const [confirmPassword, SetconfirmPassword] = useState("");
+  const [emailMsg, SetEmailMsg] = useState("");
+  const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   function cadastrar(e) {
     e.preventDefault();
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        title: title,
-        body: body,
-        userId: Math.random().toString(36).slice(2),
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    });
+
+    if (
+      mailformat.test(email) &&
+      password == confirmPassword &&
+      password.length >= 8
+    ) {
+      SetEmailMsg("");
+      SetPasswordMin("");
+      axios
+        .post("http://localhost:3333/users", {
+          email: email,
+          password: password,
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          if (error.response.data == "email already in use")
+            SetEmailMsg("o e-mail já esta em uso");
+        });
+    } else {
+      if (!mailformat.test(email)) SetEmailMsg("E-mail inválido");
+      if (password.length < 8)
+        SetPasswordMin("A senha precisa ter pelo menos 8 caracteres");
+    }
   }
 
-  function errorMsg() {
-    if (password != confirmPassword) return <span>Senhas não conferem</span>;
+  function errorPasswordMsg() {
+    if (password != confirmPassword) {
+      return <span>As senhas não coincidem</span>;
+    }
   }
 
   return (
     <div className="d-flex flex-column align-items-center content-box justify-content-center">
-      <span className="title">Realize seu cadastro!</span>
+      <div className="d-flex flex-row justify-content-start title">
+        <span>Realize seu cadastro!</span>
+      </div>
 
       <form onSubmit={cadastrar}>
-        <div className="d-flex flex-column input-box">
+        <div className="d-flex flex-column input-box ">
           <label>E-mail de acesso</label>
           <input
             placeholder="Digite seu e-email"
             onChange={(e) => {
-              SetName(e.target.value);
+              setEmail(e.target.value);
             }}
           ></input>
+          <div className="error-msg">
+            <span>{emailMsg}</span>
+          </div>
         </div>
         <div className="d-flex flex-column input-box">
           <label>Senha</label>
@@ -47,6 +72,9 @@ function SignUp() {
               Setpassword(e.target.value);
             }}
           ></input>
+          <div className="d-flex flex-column error-msg">
+            <span>{passwordMin}</span>
+          </div>
         </div>
         <div className="d-flex flex-column input-box">
           <label>Confirme sua senha</label>
@@ -57,9 +85,9 @@ function SignUp() {
               SetconfirmPassword(e.target.value);
             }}
           ></input>
-          <div className="error-msg">{errorMsg()}</div>
+          <div className="error-msg">{errorPasswordMsg()}</div>
         </div>
-        <div className="d-flex flex-row justify-content-end button-row">
+        <div className="d-flex flex-row justify-content-center">
           <button className="button-box">Cadastrar</button>
         </div>
       </form>
